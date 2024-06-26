2a59b2e42f4375861c245103d7259b0a8449c70d
CristianoArcuri <146969515+CristianoArcuri@users.noreply.github.com>
2024-06-26 15:10:31 +0200

    Create Rand.h

---
+++ Rand.h
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <fstream>
+#include <cmath>
+#include <cstdlib>
+#include <cstring>
+
+
+using namespace std;
+
+#ifndef Punto_RAND_H_
+#define Punto_RAND_H_
+
+double rand_uniforme(double , double );
+
+double rand_gaussiano(double , double );
+
+void impostaSeme();
+
+
+
+
+#endif
